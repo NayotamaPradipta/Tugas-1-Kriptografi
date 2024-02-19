5414699e940e4b5d01869bd749bae58b427adfea
Nayotama Pradipta <n4y0t4m4@gmail.com>
2024-02-20 01:53:41 +0700

    feat: add Playfair cipher encryption algorithm

--- lib/ciphers/playfair_cipher.rb
+++ lib/ciphers/playfair_cipher.rb
@@ -1,13 +1,91 @@
+require 'matrix'
 module Ciphers 
     class PlayfairCipher
         def self.encrypt(text, key)
-            "Playfair"
-            # TODO: Add Playfair cipher encryption
+            cipher_text = ""
+            # Matrix key 5x5
+            m_key = transform_key(key)
+            # Convert text to bigram
+            b_text = transform_text(text)
+
+            b_text.each do |bigram|
+                idx_1 = get_char_index(m_key, bigram[0])
+                idx_2 = get_char_index(m_key, bigram[1])
+                # Same row
+                if idx_1[0] == idx_2[0]
+                    cipher_text << m_key[idx_1[0], (idx_1[1]+1) % 5]
+                    cipher_text << m_key[idx_2[0], (idx_2[1]+1) % 5]
+                # Same column
+                elsif idx_1[1] == idx_2[1]
+                    cipher_text << m_key[(idx_1[0]+1) % 5, idx_1[1]]
+                    cipher_text << m_key[(idx_2[0]+1) % 5, idx_1[1]]
+                else 
+                    cipher_text << m_key[idx_1[0], idx_2[1]]
+                    cipher_text << m_key[idx_2[0], idx_1[1]]
+                end 
+            end 
+            cipher_text
         end
 
         def self.decrypt(text, key)
             "Playfair"
             # TODO: Add Playfair cipher decryption
+        end
+
+        def self.transform_key(key)
+            # Create array of alphabet excluding J
+            alphabet = ('A'..'Z').to_a.delete_if { |char| char == 'J'}
+            # Delete J
+            key = key.upcase.delete('J')
+            # Delete duplicate characters
+            key = key.chars.uniq.join
+            # Delete whitespace
+            key = key.delete(' ')
+            # Add the rest of alphabet
+            alphabet.each do |char|
+                key += char unless key.include?(char)
+            end
+            # Transform into 5x5 matrix
+            key_chars = key.chars.each
+            m_key = Matrix.build(5,5){key_chars.next}
+            m_key
+        end
+
+        def self.transform_text(text)
+            # Delete all space
+            text = text.delete(' ')
+            # Change to uppercase
+            text = text.upcase 
+            # Change j to i
+            text = text.gsub('J', 'I')
+            bigrams = []
+            index = 0
+            while index < text.length
+                # Insert X in between same characters
+                if text[index] == text[index+1]
+                    bigrams << text[index] + 'X'
+                    index += 1
+                else 
+                    if (text[index+1] != nil)
+                        bigrams << text[index, 2]
+                    else 
+                        # Add X if the last bigram only consists of one character
+                        bigrams << text[index] + 'X'
+                    end
+                    index+=2
+                end
+            end
+            bigrams
+        end
+
+        def self.get_char_index(m_key, character)
+            (0...5).each do |i|
+                (0...5).each do |j|
+                    if m_key[i, j] == character
+                        return [i, j]
+                    end 
+                end 
+            end
         end
     end 
 end
